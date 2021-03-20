a8b8d39a32aa49f5913049266ae85b34ad9bf36c
Mehmet Ali Summak <alisummak.8@gmail.com>
2021-03-20 15:24:42 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,99 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if ((car.Description).Length < 2)
+            {
+                Console.Write("Araç adı en az 2 karakter olmalı");
+                if (car.DailyPrice <= 0)
+                {
+                    Console.WriteLine(",");
+                    Console.WriteLine("Aracın günlük fiyatı 0'dan büyük olmalı.");
+                }
+                else
+                {
+                    Console.WriteLine(".");
+                }
+            }
+
+            else if (car.DailyPrice <= 0)
+            {
+                Console.WriteLine("Aracın günlük fiyatı 0'dan büyük olmalı.");
+            }
+
+            else if ((car.Description).Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public Car Get(int id)
+        {
+            return _carDal.Get(c=>c.Id==id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c=>c.BrandId==brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
+        }
+
+        public void Update(Car car)
+        {
+            if ((car.Description).Length < 2)
+            {
+                Console.Write("Araç adı en az 2 karakter olmalı");
+                if (car.DailyPrice <= 0)
+                {
+                    Console.WriteLine(",");
+                    Console.WriteLine("Aracın günlük fiyatı 0'dan büyük olmalı.");
+                }
+                else
+                {
+                    Console.WriteLine(".");
+                }
+            }
+
+            else if (car.DailyPrice <= 0)
+            {
+                Console.WriteLine("Aracın günlük fiyatı 0'dan büyük olmalı.");
+            }
+
+            else if ((car.Description).Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Update(car);
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            ////Add
+            //carManager.Add(new Car {Id=,BrandId=,ColorId=,Description="",DailyPrice=,ModelYear=});
+
+            ////Update
+            //carManager.Update(new Car {Id=,ColorId=,BrandId=,Description="",DailyPrice=,ModelYear=});
+
+            ////Delete
+            //carManager.Delete(new Car {Id=});
+
+            ////Get
+            //var receivedCar = carManager.Get(/*Id=*/);
+            //Console.WriteLine(receivedCar.Description);
+
+            ////GetAll
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            ////GetByColorId
+            //foreach (var car in carManager.GetCarsByColorId(2))
+            //{
+            //    Console.WriteLine(car.BrandId + car.Description);
+            //}
+
+            ////GetByBrandId
+            //foreach (var car in carManager.GetCarsByBrandId(1))
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+        }
+    }
+}
